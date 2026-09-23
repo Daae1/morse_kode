f5f25d100a25d948838be4dfb891e8ad83d876fe
malte <zbc25mshd@zbc.dk>
2026-09-23 12:24:05 +0200

    Morsekode dictionary

--- morse.py
+++ morse.py
@@ -1,8 +1,37 @@
 # Dictionary til oversættelse fra bogstaver til morsekode
-morseCode = {}
+morseCode = {'a':'.-'
+    ,'b':'-...'
+    ,'c':'-.-.'
+    ,'d':'-..'
+    ,'e':'.'
+    ,'f':'..-.'
+    ,'g':'--.'
+    ,'h':'....'
+    ,'i':'..'
+    ,'j':'.---'
+    ,'k':'-.-'
+    ,'l':'.-..'
+    ,'m':'--'
+    ,'n':'-.'
+    ,'o':'---'
+    ,'p':'.--.'
+    ,'q':'--.-'
+    ,'r':'.-.'
+    ,'s':'...'
+    ,'t':'-'
+    ,'u':'..-'
+    ,'v':'...-'
+    ,'w':'.--'
+    ,'x':'-..-'
+    ,'y':'-.--'
+    ,'z':'--..'
+    ,'æ':'.-.-'
+    ,'ø':'---.'
+    ,'å':'.--.-'
+             }
 
 # Dictionary til oversættelse fra morsekode til bogstaver. Tomt oversættes til mellemrum.
-morseCodeReverse = {}
+morseCodeReverse = {'.-:a,-...:b'}
 
 # Denne funktion oversætter et enkelt bogstav (letter) med opslag i dictionay (code) hvis muligt
 def translate(letter, code):
